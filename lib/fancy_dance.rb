c0dd7465fe583c67067549e14f11834b2cc81e32
Mikael Kaya <kayaswork@gmail.com>
2020-01-14 16:58:44 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -26,3 +26,15 @@ module FancyDance
   end
 end
  
+ 
+ angelina = Dancer.new
+angelina.twirl
+// returns "I'm twirling!"
+angelina.jump
+// returns "Look how high I'm jumping!"
+ 
+buster = Kid.new
+buster.jump
+// returns "Look how high I'm jumping!"
+buster.take_a_bow
+// returns "Thank you, thank you. It was a pleasure to dance for you all."
